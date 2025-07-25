dc13a553d8540cd048fa097f8b2db9626ed034d1
Aileen127 <fleuraileen394@gmail.com>
2025-07-25 09:47:31 -0600

    fin programa

--- actividad08.py
+++ actividad08.py
@@ -13,17 +13,31 @@ while True:
 
     match option:
         case "1":
-            producto = input("Ingresa el nombre del producto que deseas agregar")
+            producto = input("Ingresa el nombre del producto que deseas agregar: ")
             productos.append(producto)
             print("El producto ha sido agregado")
 
         case "2":
             print(f"Lista de productos actual: {productos} ")
-            indice = input("Ingresa el indice del producto que deseas modificar (el conteo inicia en 0): ")
-            nuevo_valor = input("Ingresa el nuevo producto")
+            indice = int(input("Ingresa el indice del producto que deseas modificar (el conteo inicia en 0): "))
+            nuevo_valor = input("Ingresa el nuevo producto: ")
             productos[indice] = nuevo_valor
             print("Se ha agregado el elemento actual")
 
         case "3":
             print(f"Lista de productos actual: {productos}")
-            producto_eliminar = input("Ingresa el nombre del producto que deseas eliminar")
+            producto_eliminar = input("Ingresa el nombre del producto que deseas eliminar: ").lower()
+            if producto_eliminar in productos:
+                productos.remove(producto_eliminar)
+            else:
+                print("El producto no se encuentra en la lista")
+
+        case "4":
+            print(f"Lista actual {productos}")
+
+        case "5":
+            print("Ha salido del programa")
+            break
+        case _:
+            print("Opción inválida,  intente de nuevo")
+
