cf0849fbbdd6e77400ef2d314723d45e85c2e161
Aileen127 <fleuraileen394@gmail.com>
2025-07-25 09:51:47 -0600

    no era fin del progama, modificacion de errores

--- actividad08.py
+++ actividad08.py
@@ -12,18 +12,24 @@ while True:
     option = input("Ingresa una opcion por favor: ")
 
     match option:
+        # Agregar producto
         case "1":
             producto = input("Ingresa el nombre del producto que deseas agregar: ")
             productos.append(producto)
             print("El producto ha sido agregado")
 
+        # Modificar productos
         case "2":
-            print(f"Lista de productos actual: {productos} ")
-            indice = int(input("Ingresa el indice del producto que deseas modificar (el conteo inicia en 0): "))
-            nuevo_valor = input("Ingresa el nuevo producto: ")
-            productos[indice] = nuevo_valor
-            print("Se ha agregado el elemento actual")
+            if len(productos) > 0:
+                print(f"Lista de productos actual: {productos} ")
+                indice = int(input("Ingresa el indice del producto que deseas modificar (el conteo inicia en 0): "))
+                nuevo_valor = input("Ingresa el nuevo producto: ")
+                productos[indice] = nuevo_valor
+                print("Se ha agregado el elemento actual")
+            else:
+                print("No hay productos en la lista")
 
+        # ELiminar producto
         case "3":
             print(f"Lista de productos actual: {productos}")
             producto_eliminar = input("Ingresa el nombre del producto que deseas eliminar: ").lower()
@@ -32,12 +38,16 @@ while True:
             else:
                 print("El producto no se encuentra en la lista")
 
+        # Ver todos los productos
         case "4":
             print(f"Lista actual {productos}")
 
+        # Salida del programa
         case "5":
             print("Ha salido del programa")
             break
+
+        # Por si el usuario se equivoca
         case _:
             print("Opción inválida,  intente de nuevo")
 
